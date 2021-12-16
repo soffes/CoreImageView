b6382c5cba5b59cf44f2570b552cc3fc2b6b2e0c
Sam Soffes <sam@soff.es>
2021-12-15 17:05:54 -0800

    Add example macOS app

---
+++ Example/Example/AppDelegate.swift
@@ -0,0 +1,4 @@
+import AppKit
+
+@main
+final class AppDelegate: NSObject, NSApplicationDelegate {}
---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,27 @@
+import AppKit
+import CoreImageView
+
+final class ViewController: NSViewController {
+    // MARK: - Properties
+
+    private let imageView = CoreImageView()
+
+    // MARK: - NSViewController
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            imageView.topAnchor.constraint(equalTo: view.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+
+        let image = NSImage(named: "test")!
+        imageView.image = CIImage(cgImage: image.cgImage(forProposedRect: nil, context: nil, hints: nil)!)
+    }
+}
